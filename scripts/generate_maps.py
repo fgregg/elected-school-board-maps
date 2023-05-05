3917ccf42153d9b3fb2bd9c7916f2114f4211d19
Forest Gregg <fgregg@datamade.us>
2023-05-05 11:14:58 -0400

    genetic algorithm, closes #6

--- scripts/generate_maps.py
+++ scripts/generate_maps.py
@@ -1,59 +1,29 @@
 import functools
 import json
 import math
-import random
-import sys
 
 import click
-from gerrychain import GeographicPartition, Graph, MarkovChain, constraints
+import tqdm
+from gerrychain import GeographicPartition, Graph, MarkovChain, constraints, accept
 from gerrychain.proposals import recom
 from gerrychain.updaters import Tally, cut_edges
 
 
 def std(values, scaling):
-    return math.sqrt(sum((val / scaling - 1) ** 2 for val in values)) / len(values)
+    return math.sqrt(sum((val - scaling) ** 2 for val in values)) / len(values)
 
 
 def max_diff(values, scaling):
     return max(abs(val / scaling - 1) for val in values)
 
 
-def substantially_similar_kids(ideal_public_school_kids):
-    previous_max = float("inf")
-    previous_sd = float("inf")
-
-    def smaller_differences(partition):
-        nonlocal previous_max
-        nonlocal previous_sd
-        current = max_diff(
-            partition["public_school_kids"].values(), ideal_public_school_kids
-        )
-        sd = max_diff(
-            partition["public_school_kids"].values(), ideal_public_school_kids
-        )
-
-        if current < previous_max or (current == previous_max and sd < previous_sd):
-            previous_max = current
-            previous_sd = sd
-            return 1.0
-
-        elif random.random() < 0.20:
-            previous_max = current
-            previous_sd = sd
-            return 1.0
-
-        else:
-            return 0.0
-
-    return smaller_differences
-
-
 @click.command()
+@click.option("--n_epochs", type=int, default=10)
+@click.option("--n_mutations", type=int, default=10)
+@click.option("--n_seeds", type=int, default=5)
+@click.option("--n_steps", type=int, default=20)
 @click.argument("input_geojson")
-@click.option("-s", "--steer", is_flag=True, default=False)
-@click.option("-c", "--constraint", type=float, default=float("inf"))
-@click.option("-e", "--early", type=float, default=0.0)
-def main(input_geojson, steer, constraint, early):
+def main(input_geojson, n_epochs, n_mutations, n_seeds, n_steps):
     graph = Graph.from_file(input_geojson, ignore_errors=True)
 
     initial_partition = GeographicPartition(
@@ -71,6 +41,7 @@ def main(input_geojson, steer, constraint, early):
     ideal_population = sum(initial_partition["population"].values()) / len(
         initial_partition
     )
+
     proposal = functools.partial(
         recom,
         pop_col="p1_001n",
@@ -90,52 +61,41 @@ def main(input_geojson, steer, constraint, early):
         initial_partition["public_school_kids"].values()
     ) / len(initial_partition)
 
-    kid_constraint = (
-        lambda partition: max_diff(
-            partition["public_school_kids"], ideal_public_school_kids
-        )
-        < constraint
-    )
-
-    if steer:
-        accept = substantially_similar_kids(ideal_public_school_kids)
-    else:
-        accept = lambda partition: 1.0
-
-    chain = MarkovChain(
-        proposal=proposal,
-        constraints=[
-            compactness_bound,
-            pop_constraint,
-            kid_constraint,
-        ],
-        accept=accept,
-        initial_state=initial_partition,
-        total_steps=1000,
-    )
-
-    for partition in chain:
-        click.echo(
-            std(partition["public_school_kids"].values(), ideal_public_school_kids),
-            err=True,
-        )
-        diff = max_diff(
+    def sort_key(partition):
+        return max_diff(
             partition["public_school_kids"].values(), ideal_public_school_kids
         )
 
-        click.echo(diff, err=True)
-        click.echo(
-            {k: int(v) for k, v in sorted(partition["public_school_kids"].items())},
-            err=True,
-        )
-        if diff < early:
-            with open(sys.argv[1]) as f:
-                blocks = json.load(f)
-            for i, feature in enumerate(blocks["features"]):
-                feature["properties"]["district"] = partition.assignment[i]
+    seeds = [initial_partition]
 
-            click.echo(json.dumps(blocks))
-            break
+    for epoch in tqdm.trange(n_epochs, desc="    epoch", position=0):
+        children = []
+        for seed in tqdm.tqdm(seeds, desc="     seed", leave=False, position=1):
+            for _ in tqdm.trange(
+                n_mutations, desc=" mutation", leave=False, position=2
+            ):
+                chain = MarkovChain(
+                    proposal=proposal,
+                    constraints=[compactness_bound, pop_constraint],
+                    accept=accept.always_accept,
+                    initial_state=seed,
+                    total_steps=n_steps,
+                )
+
+                children.extend([partition for partition in chain])
+
+        seeds = sorted(children, key=sort_key)[:n_seeds]
+        click.echo([sort_key(partition) for partition in seeds], err=True)
+
+        best_partition = seeds[0]
+        with open(input_geojson) as f:
+            blocks = json.load(f)
+
+        for i, feature in enumerate(blocks["features"]):
+            feature["properties"]["district"] = best_partition.assignment[i]
+
+        with open(f"epoch_{epoch}.geojson", "w") as f:
+            json.dump(blocks, f)
 
 
 if __name__ == "__main__":
