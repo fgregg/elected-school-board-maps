e2aca87ee08daef40fbe1ded97833aa7673a1147
Forest Gregg <fgregg@datamade.us>
2023-05-15 00:30:44 -0400

    revert may 5 script

--- scripts/may_5_draft_plan.sql
+++ scripts/may_5_draft_plan.sql
@@ -18,6 +18,15 @@ WITH btt AS (
                 f_table_name = 'blocks_2020'
                 AND search_frame = tract.geometry)
 ),
+tract_pop AS (
+    SELECT
+        tract_id,
+        sum(block_tract_pop) AS tract_pop
+    FROM
+        btt
+    GROUP BY
+        tract_id
+),
 btp AS (
     SELECT
         precinct.ogc_fid AS precinct_id,
@@ -40,29 +49,31 @@ block_level AS (
         block_id,
         p1_001n,
         senate_plan.Name AS district_name,
-        sum("ja_mal green" * (block_precinct_pop / p1_001n)) AS "ja_mal green",
-        sum("sophia king" * (block_precinct_pop / p1_001n)) AS "sophia king",
-        sum("kam buckner" * (block_precinct_pop / p1_001n)) AS "kam buckner",
-        sum("willie l. wilson" * (block_precinct_pop / p1_001n)) AS "willie l. wilson",
-        sum("brandon johnson" * (block_precinct_pop / p1_001n)) AS "brandon johnson",
-        sum("paul vallas" * (block_precinct_pop / p1_001n)) AS "paul vallas",
-        sum("lori e. lightfoot" * (block_precinct_pop / p1_001n)) AS "lori e. lightfoot",
-        sum("roderick t. sawyer" * (block_precinct_pop / p1_001n)) AS "roderick t. sawyer",
-        sum([jesus "chuy" garcia] * (block_precinct_pop / p1_001n)) AS[jesus "chuy" garcia],
-        sum("registered voters" * (block_precinct_pop / p1_001n)) AS "registered voters",
-        sum("ballots cast" * (block_precinct_pop / p1_001n)) AS "ballots cast",
-        sum(school_age_public * (block_tract_pop / p1_001n)) AS school_age_public,
-        sum(total_cvap * (block_tract_pop / p1_001n)) AS total_cvap,
-        sum(white_cvap * (block_tract_pop / p1_001n)) AS white_cvap,
-        sum(black_cvap * (block_tract_pop / p1_001n)) AS black_cvap,
-        sum(native_american_cvap * (block_tract_pop / p1_001n)) AS native_american_cvap,
-        sum(asian_cvap * (block_tract_pop / p1_001n)) AS asian_cvap,
-        sum(pacific_islander_cvap * (block_tract_pop / p1_001n)) AS pacific_islander_cvap,
-        sum(latino_cvap * (block_tract_pop / p1_001n)) AS latino_cvap,
+        sum("ja_mal green" * (block_precinct_pop / precinct_pop)) AS "ja_mal green",
+        sum("sophia king" * (block_precinct_pop / precinct_pop)) AS "sophia king",
+        sum("kam buckner" * (block_precinct_pop / precinct_pop)) AS "kam buckner",
+        sum("willie l. wilson" * (block_precinct_pop / precinct_pop)) AS "willie l. wilson",
+        sum("brandon johnson" * (block_precinct_pop / precinct_pop)) AS "brandon johnson",
+        sum("paul vallas" * (block_precinct_pop / precinct_pop)) AS "paul vallas",
+        sum("lori e. lightfoot" * (block_precinct_pop / precinct_pop)) AS "lori e. lightfoot",
+        sum("roderick t. sawyer" * (block_precinct_pop / precinct_pop)) AS "roderick t. sawyer",
+        sum([jesus "chuy" garcia] * (block_precinct_pop / precinct_pop)) AS[jesus "chuy" garcia],
+        sum("registered voters" * (block_precinct_pop / precinct_pop)) AS "registered voters",
+        sum("ballots cast" * (block_precinct_pop / precinct_pop)) AS "ballots cast",
+        sum(school_age_public * (block_tract_pop / tract_pop)) AS school_age_public,
+        sum(total_cvap * (block_tract_pop / tract_pop)) AS total_cvap,
+        sum(white_cvap * (block_tract_pop / tract_pop)) AS white_cvap,
+        sum(black_cvap * (block_tract_pop / tract_pop)) AS black_cvap,
+        sum(native_american_cvap * (block_tract_pop / tract_pop)) AS native_american_cvap,
+        sum(asian_cvap * (block_tract_pop / tract_pop)) AS asian_cvap,
+        sum(pacific_islander_cvap * (block_tract_pop / tract_pop)) AS pacific_islander_cvap,
+        sum(latino_cvap * (block_tract_pop / tract_pop)) AS latino_cvap,
         block.geometry
     FROM
         btt
         INNER JOIN btp USING (block_id)
+        INNER JOIN tract_pop USING (tract_id)
+        INNER JOIN precinct_pop USING (precinct_id)
         INNER JOIN municipal_general_2023 AS precinct ON precinct_id = precinct.ogc_fid
         INNER JOIN blocks_2020 AS block ON block_id = block.ogc_fid
         INNER JOIN public_school_cvap AS tract ON tract_id = tract.ogc_fid
